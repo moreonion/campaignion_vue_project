913fba2b61db361b76112ed385c72072328fe1de
Maya Kokits <maya@more-onion.com>
2017-08-09 11:01:21 +0200

    Remove .min from css filename.

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -42,7 +42,7 @@ var webpackConfig = merge(baseWebpackConfig, {
     }),
     // extract css into its own file
     new ExtractTextPlugin({
-      filename: utils.assetsPath('../../../css/my_vue_app/[name].min.css')
+      filename: utils.assetsPath('../../../css/my_vue_app/[name].css')
     }),
     // Compress extracted CSS. We are using this plugin so that possible
     // duplicated CSS from different components can be deduped.
